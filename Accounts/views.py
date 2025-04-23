5fe5892ad97e182980e156ed9b491eb28990495d
zn-har <zenharmuhammed00@gmail.com>
2025-04-23 21:37:48 +0530

    Updated.

--- Accounts/views.py
+++ Accounts/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 
 def login(request):
     return render(request,'login/login.html')
+def signup(request):
+    return render(request, 'login/signup.html')
 # Create your views here.
