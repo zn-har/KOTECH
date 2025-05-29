cd3efb47b5f0f866b709da8b5f9c5df21504f007
zn-har <zenharmuhammed00@gmail.com>
2025-05-29 14:41:25 +0530

    minor

--- Accounts/models.py
+++ Accounts/models.py
@@ -105,7 +105,7 @@ class Event(models.Model):
     venue = models.ForeignKey(Venue, on_delete=models.CASCADE)
     image = models.ImageField(upload_to="images/", null=True, blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-
+    redirect = models.CharField( max_length=100, default="")
     def __str__(self):
         return self.name
 
